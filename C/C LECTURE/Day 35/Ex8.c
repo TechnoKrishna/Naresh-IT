9986534a9e25b2bfe784c784343330dd56f9583f
TechnoKrishna <krishna.sonawane03@gmail.com>
2025-08-13 19:41:19 +0530

    Practice Question

---
+++ C/C LECTURE/Day 35/Ex8.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main() {
+
+    printf("Krishna\n", "Naidu");
+    printf("Krishna %s\n", "Sonawane");
+    printf("%s kiya %s\n", "Mine", "Pyar");
+    printf("Kahona %s %s\n", "Pyar %s", "Hai");
+
+    return 0;
+}
