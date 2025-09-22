c14d0d776d49ab7b70b326e84bbeef3eea6ce8b0
TechnoKrishna <krishna.sonawane03@gmail.com>
2025-09-23 01:50:04 +0530

    Integral  Literal

---
+++ Java/Lecture/Day 11 Integral Literal/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        accept((byte)20);  
+    }
+
+    public static void accept(byte b) {
+        System.out.println("Byte value: " + b);
+    }
+}
