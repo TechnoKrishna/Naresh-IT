8823fb12c91e464c738d24c4e4a78dd581a91bb0
TechnoKrishna <krishna.sonawane03@gmail.com>
2025-09-29 23:47:20 +0530

    Operators

---
+++ Java/Lecture/Day 16 Operators/PracticeQuestion.java
@@ -0,0 +1,381 @@
+public class PracticeQuestion {
+    public static void main(String[] args) {
+
+        // byte b1 = 10;
+        // byte b2 = 20;
+        // System.out.println(b1+b2);
+
+        // byte b1 = 10;
+        // byte b2 = 20;
+        // byte result = b1 + b2;
+        // System.out.println(b1+b2);
+
+        // byte b1 = 10;
+        // byte b2 = 20;
+        // byte result = (byte)b1 + b2;
+        // System.out.println(result);
+
+        // byte b1 = 10;
+        // byte b2 = 20;
+        // byte result = (byte)b1 + (byte)b2;
+        // System.out.println(result);
+
+        // byte b1 = 10;
+        // byte b2 = 20;
+        // byte result = (byte)(b1 + b2);
+        // System.out.println(result);
+
+        // byte b1 = 10;
+        // byte b2 = 20;
+        // byte result = 10+20;
+        // System.out.println(result);
+
+        // byte b1 = 10;
+        // byte b2 = 20;
+        // byte result = 110+120;
+        // System.out.println(result);
+
+        // byte b1 = 10;
+        // byte b2 = 20;
+        // byte result = 10+20;
+        // System.out.println(result);
+
+        // byte b1 = 10L;
+        // byte b2 = 20;
+        // byte result = b1+b2;
+        // System.out.println(result);
+
+        // byte b1 = (byte)10L;
+        // byte b2 = 20;
+        // byte result = b1+b2;
+        // System.out.println(result);
+
+        // byte b1 = (int)10L;
+        // byte b2 = 20;
+        // byte result = b1+b2;
+        // System.out.println(result);
+
+        // long b1 = 10L;
+        // byte b2 = b1;
+        // byte result = b1+b2;
+        // System.out.println(result);
+
+        // long b1 = 10L;
+        // byte b2 = (byte)b1;
+        // byte result = b1+b2;
+        // System.out.println(result);
+
+        // long b1 = 10L;
+        // byte b2 = (int)b1;
+        // byte result = b1+b2;
+        // System.out.println(result);
+
+        // byte b1 = 10;
+        // byte b2 = 20;
+        // byte result = b1 + 20;
+        // System.out.println(result);
+
+        // final byte b1 = 10;
+        // byte b2 = b1+10;
+        // byte result = b2;
+        // System.out.println(result);
+
+        // final byte b1 = 10;
+        // final byte b2 = 20;
+        // byte result = b1 + b2;
+        // System.out.println(result);
+
+        // final byte b1;
+        // final byte b2;
+        // b1= 10;
+        // b2= 20;
+        // byte result = b1 + b2;
+        // System.out.println(result);
+
+        // int i1 = 10;
+        // int i2 = 20;
+        // int result = i1+i2;
+        // System.out.println(result);
+
+        // int i1 = 10;
+        // String i2 = "20";
+        // int result = i1+i2;
+        // System.out.println(result);
+
+        // int i1 = 10;
+        // String i2 = "20";
+        // String result = i1+i2;
+        // System.out.println(result);
+
+        // String i1 = "10"+"JB";
+        // String i2 = "20";
+        // String result = i1+i2;
+        // System.out.println(result);
+
+        // int i1 = "10";
+        // String i2 = "20";
+        // String result = i1+i2;
+        // System.out.println(result);
+
+        // int i1 = "10";
+        // String i2 = "20";
+        // String result = i1-i2;
+        // System.out.println(result);
+
+        // String i1 = "10";
+        // String i2 = "20";
+        // String result = i1*i2;
+        // System.out.println(result);
+
+        // String i1 = "10";
+        // String i2 = "20";
+        // String result = i1/i2;
+        // System.out.println(result);
+
+        // String i1 = "10";
+        // String i2 = "20";
+        // String result = i1%i2;
+        // System.out.println(result);
+
+        // char i1 = "10";
+        // String i2 = "20";
+        // String result = i1+i2;
+        // System.out.println(result);
+
+        // char i1 = "10";
+        // String i2 = "20";
+        // String result = i1+i2;
+        // System.out.println(result);
+
+        // char i1 = '10';
+        // String i2 = "20";
+        // String result = i1+i2;
+        // System.out.println(result);
+
+        // char i1 = 'A';
+        // String i2 = "20";
+        // String result = i1+i2;
+        // System.out.println(result);
+
+        // char i1 = 'A';
+        // char i2 = "A";
+        // String result = i1+i2;
+        // System.out.println(result);
+
+        // char i1 = 'J';
+        // char i2 = 'B';
+        // char result = i1+i2;
+        // System.out.println(result);
+
+        // char i1 = 'J';
+        // char i2 = 'B';
+        // String result = i1+i2;
+        // System.out.println(result);
+
+        // char i1 = (char)"J";
+        // char i2 = 'B';
+        // char result = i1+i2;
+        // System.out.println(result);
+
+        // char i1 = (char)"J";
+        // char i2 = (char)"B";
+        // char result = i1+i2;
+        // System.out.println(result);
+
+        // char i1 = (char)'J';
+        // char i2 = (char)'B';
+        // char result = i1+i2;
+        // System.out.println(result);
+
+        // char i1 = (int)'J';
+        // char i2 = (int)'B';
+        // char result = i1+i2;
+        // System.out.println(result);
+
+        // char i1 = (int)'J';
+        // char i2 = (int)'B';
+        // int result = i1+i2;
+        // System.out.println(result);
+
+        // char i1 = (int)'J';
+        // char i2 = (int)'B';
+        // char result = (int)(i1+i2);
+        // System.out.println(result);
+
+        // char i1 = ()'J';
+        // char i2 = ()'B';
+        // char result = i1+i2;
+        // System.out.println(result);
+
+        // char i1 = ()'J';
+        // char i2 = ()'B';
+        // int result = (char)i1+i2;
+        // System.out.println(result);
+
+        // char i1 = 65;
+        // char i2 = 65;
+        // char result = i1+i2;
+        // System.out.println(result);
+
+        // char i1 = 65;
+        // char i2 = 65;
+        // char result = i1+i2;
+        // System.out.println(result);
+
+        // char i1 = 'a';
+        // char i2 = 'b';
+        // char result = (char)(i1+i2);
+        // System.out.println(result);
+
+        // char i1 = 65;
+        // char i2 = 65;
+        // char result = i1+i2;
+        // System.out.println((int)result);
+
+        // char i1 = 65;
+        // char i2 = 65;
+        // char result = i1+i2;
+        // System.out.println(result);
+
+        // String i1 = "";
+        // char i2 = 65;
+        // System.out.println(i1+i2);
+
+        // String i1 = "";
+        // String i2 = ""+10+20+'A';
+        // System.out.println(i1+i2);
+
+        // String s1 = 10+20;
+        // String s2 = "";
+        // String result = s1+s2;
+        // System.out.println(result);
+
+        // String s1 = 10+20+"";
+        // String s2 = "";
+        // String result = s1+s2;
+        // System.out.println(result);
+
+        // String s1 = "";
+        // String s2 = "";
+        // String result = s1+s2;
+        // System.out.println(result);
+
+        // int s1 = "";
+        // String s2 = "A";
+        // String result = s1+s2;
+        // System.out.println(result);
+
+        // int s1 = 10;
+        // int s2 = 20;
+        // String s3 = s1+s2;
+        // System.out.println(result);
+
+        // int s1 = 10;
+        // int s2 = 20;
+        // String s3 = "s1"+s2;
+        // System.out.println(s3);
+
+        // int s1 = 10;
+        // int s2 = 20;
+        // String s3 = "s1"+"s2";
+        // System.out.println(s3);
+
+        // int s1 = 10;
+        // int s2 = 20;
+        // String s3 = "s1+s2";
+        // System.out.println(s3);
+
+        // int s1 = 10;
+        // int s2 = 20;
+        // String s3 = s1+s2+"s1+s2";
+        // System.out.println(s3);
+
+        // int s1 = 10;
+        // int s2 = 20;
+        // String s3 = "s1+s2"+s1+s2;
+        // System.out.println(s3);
+
+        // int s1 = 10;
+        // int s2 = 20;
+        // String s3 = "s1+s2"+(s1+s2);
+        // System.out.println(s3);
+
+        // int s1 = 10;
+        // int s2 = 20;
+        // String s3 = "s1+s2"+(s1-s2);
+        // System.out.println(s3);
+
+        // int s1 = 10;
+        // int s2 = 20;
+        // String s3 = "s1-s2"+(s1-s2);
+        // System.out.println(s3);
+
+        // int s1 = 10;
+        // int s2 = 20;
+        // String s3 = "s1-s2"+(s1-s2);
+        // System.out.println(s3);
+
+        // int s1 = 10;
+        // int s2 = 20;
+        // System.out.println(22/7*s1*s2);
+
+        // int s1 = 22;
+        // int s2 = 7;
+        // int s3 = 10;
+        // int s4 = 10;
+        // System.out.println(s1/s2*s1*s2);
+
+        // int s1 = 0;
+        // int s2 = 7;
+        // System.out.println(s1/s2);
+
+        // int s1 = 0;
+        // int s2 = 1;
+        // System.out.println(s1/s2);
+
+        // int s1 = 0;
+        // int s2 = 1;
+        // System.out.println(s2/s1);
+
+        // int s1 = 2;
+        // int s2 = 1;
+        // System.out.println(s1/s2);
+
+        // int s1 = 0;
+        // int s2 = 1;
+        // System.out.println(s2/s1);
+
+        // int s1 = 0;
+        // int s2 = 0;
+        // System.out.println(s1/s2);
+
+        // int s1 = 2;
+        // int s2 = 1;
+        // System.out.println(s2/s1);
+
+        // double s1 = 0.0;
+        // int s2 = 1;
+        // System.out.println(s1/s2);
+
+        // double s1 = 0.0;
+        // double s2 = 1.0;
+        // System.out.println(s2/s1);
+
+        // double s1 = 0.0;
+        // int s2 = 1;
+        // System.out.println(s2/s1);
+
+        // double s1 = 1.0;
+        // int s2 = 0;
+        // System.out.println(s1/s2);
+
+        // double s1 = -1.0;
+        // int s2 = 0;
+        // System.out.println(s2/s1);
+
+        // int s1 = 0;
+        // int s2 = 0;
+        // System.out.println(s1/s2);
+
+    }
+}
