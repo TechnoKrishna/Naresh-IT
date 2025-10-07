0fd10b94a69ad0cfefa296ac2e02f511259b93b4
TechnoKrishna <krishna.sonawane03@gmail.com>
2025-10-07 18:13:28 +0530

    Day 3

---
+++ JavaScript/BroCode/03 Arithmetic Operatiors/script.js
@@ -0,0 +1,37 @@
+let a = 31;
+
+a = a + 1;
+console.log(a); // 32
+
+a = a - 1;
+console.log(a); // 31
+
+a = a % 2;
+console.log(a); // 1
+
+a = a * 10;
+console.log(a); // 10
+
+a = a / 2;
+console.log(a); // 5
+
+a++;
+console.log(a); // 6
+
+a--;
+console.log(a); // 5
+
+a = a ** 3;
+console.log(a); // 125
+
+// Operator Precedence Examples
+
+let exp1 = 1 + 2 * 3 + 4 ** 2;
+console.log(exp1);
+
+let exp2 = 12 % 5 + 8 / 2;
+console.log(exp2);
+
+let exp3 = 6 / 2 ** (2 + 5);
+console.log(exp3);
+
