b89369726a8404ddbb5a8e7bfbe8020c4c8e7fc1
TechnoKrishna <krishna.sonawane03@gmail.com>
2025-10-08 10:31:55 +0530

    Day 8

---
+++ JavaScript/BroCode/08 Math Object/script.js
@@ -0,0 +1,9 @@
+console.log(Math.round(4.6)); // → 5
+console.log(Math.floor(4.9)); // → 4
+console.log(Math.ceil(4.1)); // → 5
+console.log(Math.pow(2, 3)); // → 8
+console.log(Math.sqrt(16)); // → 4
+console.log(Math.abs(-9)); // → 9
+console.log(Math.max(3, 9, 7)); // → 9
+console.log(Math.min(3, 9, 7)); // → 3
+console.log(Math.random()); // → 0.35
