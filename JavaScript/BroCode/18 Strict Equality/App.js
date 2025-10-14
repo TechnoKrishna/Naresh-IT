9b3589f55d6435210c890e43d5b83c96d44cd588
TechnoKrishna <krishna.sonawane03@gmail.com>
2025-10-14 10:58:05 +0530

    Day 18

---
+++ JavaScript/BroCode/18 Strict Equality/App.js
@@ -0,0 +1,13 @@
+let x = 5; // assigns 5 to x
+
+console.log(5 == "5"); // true (same value after conversion)
+console.log(true == 1); // true (true becomes 1)
+
+console.log(5 === "5"); // false (number vs string)
+console.log(5 === 5); // true
+
+console.log(5 != "5"); // false (same value after conversion)
+console.log(5 != 6); // true
+
+console.log(5 !== "5"); // true (different type)
+console.log(5 !== 5); // false (same value and type)
